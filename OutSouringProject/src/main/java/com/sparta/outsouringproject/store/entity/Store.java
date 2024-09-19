8cfe683484e8e51006145ffcce651412316958f7
taeyoung kang <kty1467@naver.com>
2024-09-19 15:17:17 +0900

    일단 store 일부 작성

--- OutSouringProject/src/main/java/com/sparta/outsouringproject/store/entity/Store.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/store/entity/Store.java
@@ -1,7 +1,13 @@
 package com.sparta.outsouringproject.store.entity;
 
+import com.sparta.outsouringproject.user.entity.*;
 import jakarta.persistence.*;
 import lombok.*;
+
+
+import java.awt.*;
+import java.time.LocalTime;
+
 
 @Getter
 @Entity
@@ -14,4 +20,12 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id")
     private Long storeId;
+    private String name;
+    private Double orderAmount;
+    private LocalTime openTime;
+    private LocalTime closeTime;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
