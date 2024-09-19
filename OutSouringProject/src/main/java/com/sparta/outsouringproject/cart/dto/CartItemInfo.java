a76c9c995e3e785a22477d4f69cf3c25cd98d239
Nameless1004 <jjho1029@gmail.com>
2024-09-20 05:03:50 +0900

    feat. 장바구니 응답 dto에 메뉴 정보 추가, 다건조회 반환 변경, 오더 서비스 상태 변경하는 부분 통합, 오더 기록 추가

--- OutSouringProject/src/main/java/com/sparta/outsouringproject/cart/dto/CartItemInfo.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/cart/dto/CartItemInfo.java
@@ -2,7 +2,6 @@ package com.sparta.outsouringproject.cart.dto;
 
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,16 +11,18 @@ public class CartItemInfo {
 
     private Long cartItemId;
     private Long menuId;
+    private String menuName;
     private Long quantity;
     private Long price;
     private Long totalPrice;
 
     @Builder
-    public CartItemInfo(Long cartItemId, Long menuId, Long quantity, Long price, Long totalPrice) {
+    public CartItemInfo(Long cartItemId, Long menuId, Long quantity, Long price, Long totalPrice, String menuName) {
         this.cartItemId = cartItemId;
         this.menuId = menuId;
         this.quantity = quantity;
         this.price = price;
         this.totalPrice = price * quantity;
+        this.menuName = menuName;
     }
 }
--- OutSouringProject/src/main/java/com/sparta/outsouringproject/common/aop/OrderLogAop.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/common/aop/OrderLogAop.java
@@ -1,14 +1,11 @@
 package com.sparta.outsouringproject.common.aop;
 
-import com.sparta.outsouringproject.order.dto.OrderCreateRequestDto;
 import com.sparta.outsouringproject.order.dto.OrderCreateResponseDto;
 import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -22,17 +19,15 @@ public class OrderLogAop {
         Long storeId = orderCreateResponseDto.getStoreId();
         Long orderId = orderCreateResponseDto.getOrderId();
         LocalDateTime cur = LocalDateTime.now();
-        log.info("가게 ID: {} / 주문 ID: {} / 요청시각: {}", cur, storeId, orderId);
+        log.info("::: 신규 주문 :::\n가게 ID: {} / 주문 ID: {} / 요청시각: {}", cur, storeId, orderId);
     }
 
-    @AfterReturning("execution(* com.sparta.outsouringproject.order.service.OrderServiceImpl.acceptOrder(..)) || " +
-        "execution(* com.sparta.outsouringproject.order.service.OrderServiceImpl.startDelivery(..)) || " +
-        "execution(* com.sparta.outsouringproject.order.service.OrderServiceImpl.completeOrder(..))")
+    @AfterReturning("execution(* com.sparta.outsouringproject.order.service.OrderServiceImpl.changeOrderStatus(..))")
     public void logOrderStatusChange(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         Long storeId = (Long) args[0]; // todo: @AuthUser 추가되면 1로 바꿔야함
         Long orderId = (Long) args[1]; // todo: @AuthUser 추가되면 2로 바꿔야함
         LocalDateTime cur = LocalDateTime.now();
-        log.info("가게 ID: {} / 주문 ID: {} / 요청시각: {}", cur, storeId, orderId);
+        log.info("::: 배달상태 변경 :::\n가게 ID: {} / 주문 ID: {} / 요청시각: {}", cur, storeId, orderId);
     }
 }
