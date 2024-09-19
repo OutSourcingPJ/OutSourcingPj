9244bd46f58bc16a564191a132f23b4241efc350
rlvuddl1234 <rlvuddl1234@naver.com>
2024-09-19 13:41:50 +0900

    frist commit

---
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/OutSouringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.outsouringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutSouringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutSouringProjectApplication.class, args);
+    }
+
+}
---
+++ OutSouringProject/src/test/java/com/sparta/outsouringproject/OutSouringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.outsouringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutSouringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
