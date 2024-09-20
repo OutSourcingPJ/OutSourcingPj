05d97f17784bc4895816a31fbd8b68614723bbd8
Nameless1004 <jjho1029@gmail.com>
2024-09-20 11:31:24 +0900

    chore. 공통 response dto 빌더 삭제

--- OutSouringProject/src/main/java/com/sparta/outsouringproject/common/dto/ResponseDto.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/common/dto/ResponseDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResponseDto<T>{
     public int statusCode;
@@ -17,32 +16,19 @@ public class ResponseDto<T>{
 
 
     public static <T> ResponseDto<T> of(int statusCode, String message, T data) {
-        return ResponseDto.<T>builder()
-            .statusCode(statusCode)
-            .message(message)
-            .data(data)
-            .build();
+        return new ResponseDto<T>(statusCode, message, data);
     }
 
     public static <T> ResponseDto<T> of(int statusCode, T data) {
-        return ResponseDto.<T>builder()
-            .statusCode(statusCode)
-            .message("")
-            .data(data)
-            .build();
+        return new ResponseDto<T>(statusCode, "", data);
     }
 
     public static <T> ResponseDto<T> of(int statusCode, String message) {
-        return ResponseDto.<T>builder()
-            .statusCode(statusCode)
-            .message(message)
-            .build();
+        return new ResponseDto<T>(statusCode, "", null);
     }
 
     public static <T> ResponseDto<T> of(int statusCode) {
-        return ResponseDto.<T>builder()
-            .statusCode(statusCode)
-            .build();
+        return new ResponseDto<T>(statusCode, "", null);
     }
 }
 
