05395663abb4d6512a05256e092522ea80cae56f
taeyoung kang <kty1467@naver.com>
2024-09-20 16:15:19 +0900

    feat. 가게 단건 조회 시 메뉴 목록 나오게끔 수정

--- OutSouringProject/src/main/java/com/sparta/outsouringproject/store/dto/GetStoreResponseDto.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/store/dto/GetStoreResponseDto.java
@@ -1,11 +1,13 @@
 package com.sparta.outsouringproject.store.dto;
 
+import com.sparta.outsouringproject.menu.dto.MenuResponseDto;
 import com.sparta.outsouringproject.menu.entity.Menu;
 import com.sparta.outsouringproject.store.entity.Store;
 import lombok.*;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class GetStoreResponseDto {
@@ -13,13 +15,15 @@ public class GetStoreResponseDto {
     private Double orderAmount;
     private LocalTime openTime;
     private LocalTime closeTime;
-    private List<Menu> menuList;
+    private List<MenuResponseDto> menuList;
 
     public GetStoreResponseDto(Store store) {
         this.name = store.getName();
         this.orderAmount = store.getOrderAmount();
         this.openTime = store.getOpenTime();
         this.closeTime = store.getCloseTime();
-        this.menuList = store.getMenuList();
+        this.menuList = store.getMenuList().stream()
+                .map(menu -> new MenuResponseDto(menu.getMenuName(), menu.getMenuPrice()))
+                .collect(Collectors.toList());
     }
 }
