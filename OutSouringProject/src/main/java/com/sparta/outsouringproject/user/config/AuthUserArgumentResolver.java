cf8948aa8da05829dda6c5e7318ffd45d172a5f5
Nameless1004 <jjho1029@gmail.com>
2024-09-22 08:15:18 +0900

    feat. @Auth 아규먼트 리졸버 기능 추가 * 참고 토큰 만들 때 claim에 키 값 userRole -> role로 변경

--- OutSouringProject/src/main/java/com/sparta/outsouringproject/user/config/AuthUserArgumentResolver.java
+++ OutSouringProject/src/main/java/com/sparta/outsouringproject/user/config/AuthUserArgumentResolver.java
@@ -1,4 +1,46 @@
 package com.sparta.outsouringproject.user.config;
 
-public class AuthUserArgumentResolver {
+import com.sparta.outsouringproject.common.annotation.Auth;
+import com.sparta.outsouringproject.common.dto.AuthUser;
+import com.sparta.outsouringproject.common.exceptions.AuthException;
+import com.sparta.outsouringproject.user.entity.Role;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAuthAnnotation = parameter.getParameterAnnotation(Auth.class) != null;
+        boolean isAuthUserType = parameter.getParameterType().equals(AuthUser.class);
+
+        // @Auth 어노테이션과 AuthUser 타입이 함께 사용되지 않은 경우 예외 발생
+        if (hasAuthAnnotation != isAuthUserType) {
+            throw new AuthException("@Auth와 AuthUser 타입은 함께 사용되어야 합니다.");
+        }
+
+        return hasAuthAnnotation;
+    }
+
+    @Override
+    public Object resolveArgument(
+        @Nullable MethodParameter parameter,
+        @Nullable ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest,
+        @Nullable WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        // JwtFilter 에서 set 한 userId, email, userRole 값을 가져옴
+        Long userId = (Long) request.getAttribute("id");
+        String email = (String) request.getAttribute("email");
+        Role userRole = Role.valueOf((String)request.getAttribute("role"));
+
+        return new AuthUser(userId, email, userRole);
+    }
 }
